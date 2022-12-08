76394097fc836a07fe34e3511bc093378f23b101
FBast <bast.frederic@gmail.com>
2022-12-08 18:56:55 +0100

    Renamed boilerplate

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -7,9 +7,9 @@
   return loadTemplates([
 
     // Actor partials.
-    "systems/boilerplate/templates/actor/parts/actor-features.html",
-    "systems/boilerplate/templates/actor/parts/actor-items.html",
-    "systems/boilerplate/templates/actor/parts/actor-spells.html",
-    "systems/boilerplate/templates/actor/parts/actor-effects.html",
+    "systems/pl1e/templates/actor/parts/actor-features.html",
+    "systems/pl1e/templates/actor/parts/actor-items.html",
+    "systems/pl1e/templates/actor/parts/actor-spells.html",
+    "systems/pl1e/templates/actor/parts/actor-effects.html",
   ]);
 };
