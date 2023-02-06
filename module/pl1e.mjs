78792f1a807de891b8174b1356ba6756547eef37
FBast <bast.frederic@gmail.com>
2023-02-06 23:23:50 +0100

    Added base macro system

--- module/pl1e.mjs
+++ module/pl1e.mjs
@@ -20,7 +20,7 @@ Hooks.once('init', async function () {
     game.pl1e = {
         Pl1eActor,
         Pl1eItem,
-        rollItemMacro
+        rollItem
     };
 
     // Add custom constants for configuration.
@@ -86,66 +86,105 @@ Handlebars.registerHelper('config', function (key) {
 
 Hooks.once("ready", async function () {
     // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-    Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+    Hooks.on("hotbarDrop", (bar, data, slot) => {
+        if (["Item", "ActiveEffect"].includes(data.type) ) {
+            createMacro(data, slot);
+            return false;
+        }
+    });
 });
 
 /* -------------------------------------------- */
 /*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Attempt to create a macro from the dropped data. Will use an existing macro if one exists.
+ * @param {object} dropData     The dropped data
+ * @param {number} slot         The hotbar slot to use
+ */
+async function createMacro(dropData, slot) {
+    const macroData = { type: "script", scope: "actor" };
+    switch ( dropData.type ) {
+        case "Item":
+            const itemData = await Item.implementation.fromDropData(dropData);
+            if ( !itemData ) return ui.notifications.warn(game.i18n.localize("MACRO.5eUnownedWarn"));
+            foundry.utils.mergeObject(macroData, {
+                name: itemData.name,
+                img: itemData.img,
+                command: `game.pl1e.rollItem("${itemData.name}")`,
+                flags: {"pl1e.itemMacro": true}
+            });
+            break;
+        case "ActiveEffect":
+            const effectData = await ActiveEffect.implementation.fromDropData(dropData);
+            if ( !effectData ) return ui.notifications.warn(game.i18n.localize("MACRO.5eUnownedWarn"));
+            foundry.utils.mergeObject(macroData, {
+                name: effectData.label,
+                img: effectData.icon,
+                command: `game.pl1e.toggleEffect("${effectData.label}")`,
+                flags: {"pl1e.effectMacro": true}
+            });
+            break;
+        default:
+            return;
+    }
+
+    // Assign the macro to the hotbar
+    const macro = game.macros.find(m => (m.name === macroData.name) && (m.command === macroData.command)
+        && m.author.isSelf) || await Macro.create(macroData);
+    await game.user.assignHotbarMacro(macro, slot);
+}
 
 /* -------------------------------------------- */
 
 /**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
- * @param {number} slot     The hotbar slot to use
- * @returns {Promise}
+ * Find a document of the specified name and type on an assigned or selected actor.
+ * @param {string} name          Document name to locate.
+ * @param {string} documentType  Type of embedded document (e.g. "Item" or "ActiveEffect").
+ * @returns {Document}           Document if found, otherwise nothing.
  */
-async function createItemMacro(data, slot) {
-    // First, determine if this is a valid owned item.
-    if (data.type !== "Item") return;
-    if (!data.uuid.includes('Actor.') && !data.uuid.includes('Token.')) {
-        return ui.notifications.warn("You can only create macro buttons for owned Items");
-    }
-    // If it is, retrieve it based on the uuid.
-    const item = await Item.fromDropData(data);
+function getMacroTarget(name, documentType) {
+    let actor;
+    const speaker = ChatMessage.getSpeaker();
+    if ( speaker.token ) actor = game.actors.tokens[speaker.token];
+    actor ??= game.actors.get(speaker.actor);
+    if ( !actor ) return ui.notifications.warn(game.i18n.localize("MACRO.5eNoActorSelected"));
 
-    // Create the macro command using the uuid.
-    const command = `game.pl1e.rollItemMacro("${data.uuid}");`;
-    let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
-    if (!macro) {
-        macro = await Macro.create({
-            name: item.name,
-            type: "script",
-            img: item.img,
-            command: command,
-            flags: {"pl1e.itemMacro": true}
-        });
+    const collection = (documentType === "Item") ? actor.items : actor.effects;
+    const nameKeyPath = (documentType === "Item") ? "name" : "label";
+
+    // Find item in collection
+    const documents = collection.filter(i => foundry.utils.getProperty(i, nameKeyPath) === name);
+    const type = game.i18n.localize(`DOCUMENT.${documentType}`);
+    if ( documents.length === 0 ) {
+        return ui.notifications.warn(game.i18n.format("MACRO.5eMissingTargetWarn", { actor: actor.name, type, name }));
     }
-    game.user.assignHotbarMacro(macro, slot);
-    return false;
+    if ( documents.length > 1 ) {
+        ui.notifications.warn(game.i18n.format("MACRO.5eMultipleTargetsWarn", { actor: actor.name, type, name }));
+    }
+    return documents[0];
 }
 
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemUuid
- */
-function rollItemMacro(itemUuid) {
-    // Reconstruct the drop data so that we can load the item.
-    const dropData = {
-        type: 'Item',
-        uuid: itemUuid
-    };
-    // Load the item from the uuid.
-    Item.fromDropData(dropData).then(item => {
-        // Determine if the item loaded and if it's an owned item.
-        if (!item || !item.parent) {
-            const itemName = item?.name ?? itemUuid;
-            return ui.notifications.warn(`Could not find item ${itemName}. You may need to delete and recreate this macro.`);
-        }
+/* -------------------------------------------- */
 
-        // Trigger the item roll
-        item.roll();
-    });
+/**
+ * Trigger an item to roll when a macro is clicked.
+ * @param {string} itemName                Name of the item on the selected actor to trigger.
+ * @returns {Promise<ChatMessage|object>}  Roll result.
+ */
+function rollItem(itemName) {
+    return getMacroTarget(itemName, "Item")?.use();
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Toggle an effect on and off when a macro is clicked.
+ * @param {string} effectLabel       Label for the effect to be toggled.
+ * @returns {Promise<ActiveEffect>}  The effect after it has been toggled.
+ */
+function toggleEffect(effectLabel) {
+    const effect = getMacroTarget(effectLabel, "ActiveEffect");
+    return effect?.update({disabled: !effect.disabled});
 }
