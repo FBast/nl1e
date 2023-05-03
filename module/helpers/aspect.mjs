95938bc5531370b8a21075873f9b194c0d80b338
FBast <bast.frederic@gmail.com>
2023-05-03 21:42:02 +0200

    Fixed multiples bugs

--- module/helpers/aspect.mjs
+++ module/helpers/aspect.mjs
@@ -9,15 +9,15 @@ export class Pl1eAspect {
         // Calculate the aspect
         switch (aspect.name) {
             case "increase":
-                return this._increase(characterData, targetsData);
+                return this._increase(aspect, characterData, targetsData);
             case "decrease":
-                return this._decrease(characterData, targetsData);
+                return this._decrease(aspect, characterData, targetsData);
             case "override":
-                return this._override(characterData, targetsData);
+                return this._override(aspect, characterData, targetsData);
             case "transfer":
-                return this._transfer(characterData, targetsData);
+                return this._transfer(aspect, characterData, targetsData);
             case "effect":
-                return this._effect(characterData, targetsData);
+                return this._effect(aspect, characterData, targetsData);
             default:
                 throw new Error("PL1E | unknown aspect : " + aspect.name);
         }
@@ -25,84 +25,89 @@ export class Pl1eAspect {
 
     /**
      *
+     * @param aspect
      * @param characterData
      * @param targetsData
      * @returns {Promise<*>}
      * @private
      */
-    static async _increase(characterData, targetsData) {
+    static async _increase(aspect, characterData, targetsData) {
         for (const targetData of targetsData) {
             // Check target validation
             if (!this._isTargetValid(characterData.token, targetData.token)) continue;
 
             // Copy the aspect to calculate the new values
-            let aspect = JSON.parse(JSON.stringify(this));
-            switch (aspect.system.resolutionType) {
+            let aspectCopy = JSON.parse(JSON.stringify(aspect));
+            switch (aspect.resolutionType) {
                 case "multiplyBySuccess":
-                    aspect.system.value *= targetData.result > 0 ? targetData.result : 0;
+                    aspectCopy.value *= targetData.result > 0 ? targetData.result : 0;
                     break;
                 case "valueIfSuccess":
-                    aspect.system.value = targetData.result > 0 ? aspect.system.value : 0;
+                    aspectCopy.value = targetData.result > 0 ? aspectCopy.value : 0;
                     break;
             }
 
             // Apply the aspect
-            const aspectDataConfig = CONFIG.PL1E[aspect.system.dataGroup][aspect.system.data];
+            const aspectDataConfig = CONFIG.PL1E[aspectCopy.dataGroup][aspectCopy.data];
             let actorValue = foundry.utils.getProperty(targetData.token.actor, aspectDataConfig.path);
-            actorValue += aspect.system.value;
+            actorValue += aspectCopy.value;
             await targetData.token.actor.update({
                 [aspectDataConfig.path]: actorValue
             });
 
             // Check for existing aspect related to same function
-            targetData.aspects ??= [];
-            let existingAspect = targetData.aspects.find(aspect => aspect.system.function === this.system.function);
-            existingAspect === undefined ? targetData.aspects.push(aspect) : existingAspect.value += aspect.value;
+            targetData.activeAspects ??= [];
+            let existingAspect = targetData.activeAspects.find(aspect => aspect.name === aspectCopy.name);
+            existingAspect === undefined ? targetData.activeAspects.push(aspectCopy) : existingAspect.value += aspectCopy.value;
         }
         return targetsData;
     }
 
     /**
      *
+     * @param aspect
      * @param characterData
      * @param targetsData
      * @returns {Promise<*>}
      * @private
      */
-    static async _decrease(characterData, targetsData) {
+    static async _decrease(aspect, characterData, targetsData) {
         throw new Error("Not implemented yet");
     }
 
     /**
      *
+     * @param aspect
      * @param characterData
      * @param targetsData
      * @returns {Promise<*>}
      * @private
      */
-    static async _override(characterData, targetsData) {
+    static async _override(aspect, characterData, targetsData) {
         throw new Error("Not implemented yet");
     }
 
     /**
      *
+     * @param aspect
      * @param characterData
      * @param targetsData
      * @returns {Promise<*>}
      * @private
      */
-    static async _transfer(characterData, targetsData) {
+    static async _transfer(aspect, characterData, targetsData) {
         throw new Error("Not implemented yet");
     }
 
     /**
      *
+     * @param aspect
      * @param characterData
      * @param targetsData
      * @returns {Promise<*>}
      * @private
      */
-    static async _effect(characterData, targetsData) {
+    static async _effect(aspect, characterData, targetsData) {
         throw new Error("Not implemented yet");
     }
 
