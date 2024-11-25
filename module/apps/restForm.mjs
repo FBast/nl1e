712e567b1f0fe0f51c95870bd1fcc67b4d3863c1
Dizy <bast.frederic@gmail.com>
2024-11-26 00:08:07 +0100

    Refactored rest systm

--- module/apps/restForm.mjs
+++ module/apps/restForm.mjs
@@ -1,6 +1,8 @@
 import { Pl1eHelpers as pl1eHelpers, Pl1eHelpers } from "../helpers/helpers.mjs";
+import {Pl1eEvent} from "../helpers/events.mjs";
 
 export class RestForm extends FormApplication {
+
     constructor(actor, options = {}) {
         super(actor, options);
 
@@ -13,14 +15,16 @@ export class RestForm extends FormApplication {
         this.maxRank = this.actor.system.general.maxRank;
         this.ranks = this.actor.system.general.ranks;
 
+        this.environments = Pl1eHelpers.getConfig("environments");
         this.beds = Pl1eHelpers.getConfig("beds");
         this.meals = pl1eHelpers.getConfig("meals");
 
         // Initialize selections with options or defaults
-        this.selectedBed = options.selectedBed || Object.keys(this.beds).find(key => key === "none") || null;
-        this.selectedMeal = options.selectedMeal || Object.keys(this.meals).find(key => key === "none") || null;
+        this.selectedEnvironment = options.selectedEnvironment || Object.keys(this.beds).find(key => key === "normal");
+        this.selectedBed = options.selectedBed || Object.keys(this.beds).find(key => key === "none");
+        this.selectedMeal = options.selectedMeal || Object.keys(this.meals).find(key => key === "none");
         this.selectedBedItem = null;
-        this.selectedMealItem = null;
+        this.selectedMealItems = [];
 
         // Initialize effects
         this.effects = [];
@@ -45,14 +49,15 @@ export class RestForm extends FormApplication {
 
         const actorSheetData = await this.actor.sheet.getData();
         data.commons = actorSheetData.commons;
+        data.environments = this.environments;
         data.beds = this.beds;
         data.meals = this.meals;
 
-        // Pass current selections to the template
+        data.selectedEnvironment = this.selectedEnvironment;
         data.selectedBed = this.selectedBed;
         data.selectedMeal = this.selectedMeal;
         data.selectedBedItem = this.selectedBedItem;
-        data.selectedMealItem = this.selectedMealItem;
+        data.selectedMealItems = this.selectedMealItems;
 
         // Update effects based on current selections
         this.updateEffects();
@@ -65,7 +70,7 @@ export class RestForm extends FormApplication {
     activateListeners(html) {
         super.activateListeners(html);
 
-        const handleToggleGroup = (groupClass, stateProperty, relatedItemProperty) => {
+        const handleToggleGroup = (groupClass, stateProperty, relatedItemProperty = null) => {
             html.find(groupClass).each((index, group) => {
                 const toggleItems = $(group).find(".toggle-item");
 
@@ -81,7 +86,7 @@ export class RestForm extends FormApplication {
                     this[stateProperty] = clickedItem.data("value");
 
                     // Reset the related item property to null
-                    this[relatedItemProperty] = null;
+                    if (relatedItemProperty) this[relatedItemProperty] = Array.isArray(this[relatedItemProperty]) ? [] : null;
 
                     // Re-render to update the UI with the new selections
                     this.render();
@@ -90,27 +95,32 @@ export class RestForm extends FormApplication {
         };
 
         // Handle toggles for bed and meal groups
+        handleToggleGroup(".environment-toggle", "selectedEnvironment");
         handleToggleGroup(".bed-toggle", "selectedBed", "selectedBedItem");
-        handleToggleGroup(".meal-toggle", "selectedMeal", "selectedMealItem");
+        handleToggleGroup(".meal-toggle", "selectedMeal", "selectedMealItems");
 
         // Handle item selection
         html.find(".item-select").on("click", ev => this._onItemSelect(ev));
+        html.find(".item-edit").on("click", ev => Pl1eEvent.onItemEdit(ev, this.actor));
+        html.find(".item-tooltip-activate").on("click", ev => Pl1eEvent.onItemTooltip(ev));
     }
 
     updateEffects() {
         // Retrieve items if IDs are set
-        const mealItem = this.selectedMealItem ? this.actor.items.get(this.selectedMealItem) : null;
+        const mealItems = this.selectedMealItems.map(itemId => this.actor.items.get(itemId));
         const bedItem = this.selectedBedItem ? this.actor.items.get(this.selectedBedItem) : null;
 
         // Access attributes from selectedMeal and selectedBed
+        const environment = this.selectedEnvironment ? this.environments[this.selectedEnvironment] : null;
         const meal = this.selectedMeal ? this.meals[this.selectedMeal] : null;
         const bed = this.selectedBed ? this.beds[this.selectedBed] : null;
 
         // Calculate effects for health, mana, and stamina
         const effects = ["health", "mana", "stamina"].map(attr => {
             const value =
+                (environment?.effects[attr] || 0) + // From environment
                 (meal?.effects[attr] || 0) + // From selectedMeal
-                (mealItem?.system.attributes[`${attr}Rest`] || 0) + // From selectedMealItem
+                mealItems.reduce((sum, item) => sum + (item?.system.attributes[`${attr}Rest`] || 0), 0) +
                 (bed?.effects[attr] || 0) + // From selectedBed
                 (bedItem?.system.attributes[`${attr}Rest`] || 0); // From selectedBedItem
 
@@ -133,13 +143,26 @@ export class RestForm extends FormApplication {
         const itemType = item.system.attributes.commonType; // Determine the item's type (food/bed)
 
         if (itemType === "food") {
-            const sameMealItem = this.selectedMealItem === itemId;
-            this.selectedMealItem = sameMealItem ? null : itemId;
-            this.selectedMeal = sameMealItem ? this.selectedMeal : Object.keys(this.meals).find(key => key === "none") || null;
+            const isAlreadySelected = this.selectedMealItems.includes(itemId);
+
+            if (isAlreadySelected) {
+                // Remove the item if it's already selected
+                this.selectedMealItems = this.selectedMealItems.filter(id => id !== itemId);
+            } else {
+                // Add the new item, and remove the first if exceeding 3 items
+                this.selectedMealItems.push(itemId);
+                if (this.selectedMealItems.length > 3) {
+                    this.selectedMealItems.shift();
+                }
+            }
+
+            // Clear selectedMeal if items are selected
+            this.selectedMeal = this.selectedMealItems.length ?
+                Object.keys(this.meals).find(key => key === "none") : this.selectedMeal;
         } else if (itemType === "bed") {
             const sameBedItem = this.selectedBedItem === itemId;
             this.selectedBedItem = sameBedItem ? null : itemId;
-            this.selectedBed = sameBedItem ? this.selectedBed : Object.keys(this.beds).find(key => key === "none") || null;
+            this.selectedBed = sameBedItem ? this.selectedBed : Object.keys(this.beds).find(key => key === "none");
         }
 
         // Re-render to reflect changes
--- module/config/configRest.mjs
+++ module/config/configRest.mjs
@@ -1,13 +1,36 @@
 import {PL1E} from "../pl1e.mjs";
 
 export function getConfigRest() {
+    PL1E.environments = {
+        "normal": {
+            "img": "systems/pl1e/assets/rest/plain.jpg",
+            "tooltip": "PL1E.EnvironmentNormalTooltip",
+            "effects": {}
+        },
+        "hostile": {
+            "img": "systems/pl1e/assets/rest/taiga.jpg",
+            "tooltip": "PL1E.EnvironmentHostileTooltip",
+            "effects": {
+                "stamina": -10,
+                "mana": -10,
+            }
+        },
+        "extreme": {
+            "img": "systems/pl1e/assets/rest/desert.jpg",
+            "tooltip": "PL1E.EnvironmentExtremeTooltip",
+            "effects": {
+                "stamina": -20,
+                "mana": -20,
+            }
+        }
+    };
+
     PL1E.beds = {
         "none": {
             "img": "systems/pl1e/assets/rest/bedNone.jpg",
             "tooltip": "PL1E.BedNoneTooltip",
             "effects": {
-                "health": -5,
-                "mana": -10
+                "health": -10
             }
         },
         "poor": {
@@ -19,24 +42,21 @@ export function getConfigRest() {
             "img": "systems/pl1e/assets/rest/bedNormal.jpg",
             "tooltip": "PL1E.BedNormalTooltip",
             "effects": {
-                "health": 5,
-                "mana": 10
+                "health": 20
             }
         },
         "good": {
             "img": "systems/pl1e/assets/rest/bedGood.jpg",
             "tooltip": "PL1E.BedGoodTooltip",
             "effects": {
-                "health": 10,
-                "mana": 20
+                "health": 30
             }
         },
         "luxurious": {
             "img": "systems/pl1e/assets/rest/bedLuxurious.jpg",
             "tooltip": "PL1E.BedLuxuriousTooltip",
             "effects": {
-                "health": 15,
-                "mana": 30
+                "health": 40
             }
         }
     };
@@ -46,8 +66,8 @@ export function getConfigRest() {
             "img": "systems/pl1e/assets/rest/mealNone.jpg",
             "tooltip": "PL1E.MealNoneTooltip",
             "effects": {
-                "health": -5,
-                "stamina": -10
+                "stamina": -5,
+                "mana": -5
             }
         },
         "frugal": {
@@ -59,24 +79,24 @@ export function getConfigRest() {
             "img": "systems/pl1e/assets/rest/mealNormal.jpg",
             "tooltip": "PL1E.MealNormalTooltip",
             "effects": {
-                "health": 5,
-                "stamina": 10
+                "stamina": 10,
+                "mana": 10
             }
         },
         "good": {
             "img": "systems/pl1e/assets/rest/mealGood.jpg",
             "tooltip": "PL1E.MealGoodTooltip",
             "effects": {
-                "health": 10,
-                "stamina": 20
+                "stamina": 20,
+                "mana": 20
             }
         },
         "luxurious": {
             "img": "systems/pl1e/assets/rest/mealLuxurious.jpg",
             "tooltip": "PL1E.MealLuxuriousTooltip",
             "effects": {
-                "health": 15,
-                "stamina": 30
+                "health": 40,
+                "stamina": 40
             }
         }
     };
